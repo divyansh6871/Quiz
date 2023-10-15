21d237dfa989b455573d624c93e107b0b1b7e007
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2023-10-15 17:00:19 +0530

    Update cricket.js

--- script/cricket.js
+++ script/cricket.js
@@ -259,13 +259,13 @@ let quan = [
     },
 
     {
-        "ques": "Fastest century in ODI by Indian?",
+        "ques": "Fastest century in ODI format by an Indian?",
         "sol": { "a": "Rohit SHarma", "b": "Rishab Pant", "c": "VIrat Kohli", "d": "KL Rahul" },
         "correct": "Virat Kohli"
     },
 
     {
-        "ques": "Fastest century in ODI by Indian?",
+        "ques": "Fastest century in T20 format by an Indian?",
         "sol": { "a": "Rohit Sharma", "b": "Surya Kumar Yadav", "c": "Sursh Raina", "d": "KL Rahul" },
         "correct": "Rohit Sharma"
     },
@@ -409,8 +409,10 @@ function hidesol1() {
 
 }
 
+
 function getsol1() {
     let ans11 = document.getElementById("ans1")
+    ans11.style.display="inline"
     ans11.innerHTML = quan[index].correct
     let showanswer1 = document.getElementById("showans1")
     if (showanswer1.style.display != "none") {
@@ -419,6 +421,7 @@ function getsol1() {
 }
 function getsol2() {
     let ans22 = document.getElementById("ans2")
+    ans22.style.display="inline"
     ans22.innerHTML = quan[index1].correct
     let showanswer2 = document.getElementById("showans2")
     if (showanswer2.style.display != "none") {
@@ -428,6 +431,7 @@ function getsol2() {
 
 function getsol3() {
     let ans33 = document.getElementById("ans3")
+    ans33.style.display="inline"
     ans33.innerHTML = quan[index3].correct
     let showanswer3 = document.getElementById("showans3")
     if (showanswer3.style.display != "none") {
@@ -437,6 +441,7 @@ function getsol3() {
 
 function getsol4() {
     let ans44 = document.getElementById("ans4")
+    ans44.style.display="inline"
     ans44.innerHTML = quan[index4].correct
     let showanswer4 = document.getElementById("showans4")
     if (showanswer4.style.display != "none") {
@@ -446,6 +451,7 @@ function getsol4() {
 
 function getsol5() {
     let ans55 = document.getElementById("ans5")
+    ans55.style.display="inline"
     ans55.innerHTML = quan[index5].correct
     let showanswer5 = document.getElementById("showans5")
     if (showanswer5.style.display != "none") {
@@ -455,6 +461,7 @@ function getsol5() {
 
 function getsol6() {
     let ans66 = document.getElementById("ans6")
+    ans66.style.display="inline"
     ans66.innerHTML = quan[index6].correct
     let showanswer6 = document.getElementById("showans6")
     if (showanswer6.style.display != "none") {
@@ -464,6 +471,7 @@ function getsol6() {
 
 function getsol7() {
     let ans77 = document.getElementById("ans7")
+    ans77.style.display="inline"
     ans77.innerHTML = quan[index7].correct
     let showanswer7 = document.getElementById("showans7")
     if (showanswer7.style.display != "none") {
@@ -472,8 +480,9 @@ function getsol7() {
 }
 
 function getsol8() {
-    let ans8 = document.getElementById("ans8")
-    ans8.innerHTML = quan[index8].correct
+    let ans88 = document.getElementById("ans8")
+    ans88.style.display="inline"
+    ans88.innerHTML = quan[index8].correct
     let showanswer8 = document.getElementById("showans8")
     if (showanswer8.style.display != "none") {
         showanswer8.style.display = "none";
@@ -482,6 +491,7 @@ function getsol8() {
 
 function getsol9() {
     let ans99 = document.getElementById("ans9")
+    ans99.style.display="inline"
     ans99.innerHTML = quan[index9].correct
     let showanswer9 = document.getElementById("showans9")
     if (showanswer9.style.display != "none") {
@@ -491,9 +501,11 @@ function getsol9() {
 
 function getsol10() {
     let ans1010 = document.getElementById("ans10")
+    ans1010.style.display="inline"
     ans1010.innerHTML = quan[index10].correct
     let showanswer10 = document.getElementById("showans10")
     if (showanswer10.style.display != "none") {
         showanswer10.style.display = "none";
     }
 }
+
